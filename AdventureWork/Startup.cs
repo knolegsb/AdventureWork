f2f4f010e971b20bb4b4afb3e03a800a12f724c8
knolegsb <seon2you@hotmail.com>
2015-10-25 01:59:55 -0700

    changed start page route, login route, table design

---
+++ AdventureWork/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AdventureWork.Startup))]
+namespace AdventureWork
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
